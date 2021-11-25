79d1719e1aa135b625ecca591ffefeb3eda1b8f4
Yash sinha <sinhayash449@gmail.com>
2021-11-25 03:40:30 -0600

    Done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      primarycolor: "#F9A826",
+    },
+  },
+  fontFamily: {
+    display: ["Nunito", "sans-serif"],
+  },
+  variants: {},
+  plugins: [],
+};
